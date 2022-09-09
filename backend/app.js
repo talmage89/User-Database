355b08338a1918441d7c3b6e0476e346124299d9
Talmage Bergeson <talmage.bergeson@gmail.com>
2022-09-09 13:38:57 -0600

    first commit

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const userDataRepo = require('./database/userDataRepo');
 const dbUrl = 'mongodb://localhost:27017';
 const dbName = 'userData';
 const port = 3000;
+const originalUsers = require('./data.json');
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
@@ -15,34 +16,49 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
-app.get('/home', (req, res) => {
-    console.log('get request got');
-})
-
-app.post('/', async (req, res) => {
-    console.log("got a post")
-    console.log(req.body);
-    
+app.get('/home', async (req, res) => {
     const client = new MongoClient(dbUrl);
     await client.connect();
     try {
-        const data = [
-            {"Username": "Talmage"},
-            {"Username": "Mindy"},
-            {"Username": "Camille"}
-        ]
-        await userDataRepo.loadData(data);
+        const data = await userDataRepo.getUsers();
+        res.send(data);
     } catch (error) {
-        console.log(error);
-    } finally {
-        await client.db(dbName).dropDatabase();
-        const admin = client.db(dbName).admin();
-        console.log(await admin.listDatabases());
-        client.close();
-        res.send('Your moms')
+        console.log(error)
     }
+})
+
+app.post('/', async (req, res) => {
+    const client = new MongoClient(dbUrl);
+    await client.connect();
+        try {
+            const userToAdd = {
+                "firstName": req.body.firstName,
+                "lastName": req.body.lastName,
+                "email": req.body.email,
+                "age": req.body.age.toString()
+            };
+            let users = await userDataRepo.getUsers();
+
+            // if email is already in database
+            if (users.find(el => el.email === userToAdd.email)) {
+                await userDataRepo.addUser(userToAdd)
+                res.status(200).send();
+
+                // otherwise mark the res with an unknown email status
+            } else res.status(202).send();
+        } catch (error) {
+            console.log(error);
+        }
 });
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+app.listen(port, async () => {
+    const client = new MongoClient(dbUrl);
+    await client.connect();
+    try {
+        await client.db(dbName).dropDatabase();
+        await userDataRepo.loadData(originalUsers);
+        console.log(`server started on port ${port}, db created and populated`);
+    } catch (error) {
+        console.log(error);
+    }
 })
--- backend/database/userDataRepo.js
+++ backend/database/userDataRepo.js
@@ -1,28 +1,55 @@
-const { MongoClient, ObjectID } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 function userDataRepo() {
     const url = 'mongodb://localhost:27017';
     const dbName = 'userData';
+    const collectionName = 'users';
 
     function loadData(data) {
         return new Promise(async (resolve, reject) => {
             const client = new MongoClient(url);
             try {
                 await client.connect();
-
                 const db = client.db(dbName);
-
-                const results = await db.collection('users').insertMany(data);
+                let results = await db.collection(collectionName).insertMany(data);
                 resolve(results);
                 client.close();
-
             } catch (error) {
                 reject(error);
             }
         })
     }
 
-    return { loadData }
-}
+    function getUsers(query) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try {
+                await client.connect();
+                const db = client.db(dbName);
+                let items = await db.collection(collectionName).find(query).toArray();
+                resolve(items);
+                client.close()
+            } catch (error) {
+                reject(error);
+            }
+        })
+    }
 
+    function addUser(user) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try {
+                await client.connect();
+                const db = client.db(dbName);
+                const results = await db.collection(collectionName).insertOne(user)
+                resolve(results);
+                client.close();
+            } catch (error) {
+                reject(error);
+            }
+        })
+    }
+
+    return { loadData, addUser, getUsers }
+}
 module.exports = userDataRepo();
